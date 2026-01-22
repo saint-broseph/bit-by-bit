e8fd756d7b95ad42c5f809a64d34fe35919b56a8
saint-broseph <sahutanishq06@gmail.com>
2026-01-22 15:12:21 +0530

    Create 2sum.cpp

---
+++ DSA 25-26/Lab 1/Practice Problems/2sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        
+            unordered_map<int, int> m;  // value -> index
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (m.find(complement) != m.end()) {
+                return {m[complement], i};
+            }
+            m[nums[i]] = i;
+        }
+        return {};  // not found
+        }
+           
+};
