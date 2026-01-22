910b67651d4f8c1574ae5dbc4b3620cd4ab89375
saint-broseph <sahutanishq06@gmail.com>
2026-01-22 15:20:18 +0530

    Create apple-redistribution.cpp

---
+++ DSA 25-26/Lab 1/Practice Problems/apple-redistribution.cpp
@@ -0,0 +1,23 @@
+/* You are given an array apple of size n and an array capacity of size m.
+There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+*/
+
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        sort(capacity.begin(), capacity.end());
+        int sum=0;
+        for(int i = 0; i<apple.size(); i++)
+        sum+=apple[i];
+        int ans=0, sum2=0;
+        for(int i=capacity.size()-1; i>=0; i--)
+        {
+            sum2+=capacity[i];
+            ans++;
+            if(sum2>=sum)
+            break;
+        }
+        return ans;
+    }
+};
