5bd710d86055f100d1ab03a263917cd4174cecb7
Tanishq Sahu <sahutanishq06@gmail.com>
2026-02-12 19:58:43 +0530

    Create search-a-2d-matrix.cpp

---
+++ DSA 25-26/Lab 3/Practice Problems/search-a-2d-matrix.cpp
@@ -0,0 +1,26 @@
+/*
+  You are given an m x n integer matrix matrix with the following two properties:
+  Each row is sorted in non-decreasing order.
+  The first integer of each row is greater than the last integer of the previous row.
+  Given an integer target, return true if target is in matrix or false otherwise.
+  You must write a solution in O(log(m * n)) time complexity.
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(); int n = matrix[0].size();
+      int low = 0, high = m*n - 1;
+      while(low<=high)
+        {
+            int mid = low + (high - low)/2;
+            int r = mid / n;
+            int c = mid % n;
+
+            if(matrix[r][c] == target) return true;
+            else if (matrix[r][c] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+      return false;
+    }
+};
