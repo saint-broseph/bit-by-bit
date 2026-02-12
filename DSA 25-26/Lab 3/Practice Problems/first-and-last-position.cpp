cf8e71e69cec831367415d6bf6f183870735c3d3
Tanishq Sahu <sahutanishq06@gmail.com>
2026-02-12 19:24:24 +0530

    Update first-and-last-position.cpp

--- DSA 25-26/Lab 3/Practice Problems/first-and-last-position.cpp
+++ DSA 25-26/Lab 3/Practice Problems/first-and-last-position.cpp
@@ -1,40 +1,42 @@
-class Solution {
-public:
-int binarySearch(vector<int>& nums, int target, bool findFirst) {
-        int left = 0, right = nums.size() - 1, ans = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                if (findFirst) right = mid - 1;
-                else left = mid + 1;
-            } else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return ans;
-    }
-
-    vector<int> searchRange(vector<int>& nums, int target) {
-        return {binarySearch(nums, target, true), binarySearch(nums, target, false)};
-    }
-};
-
-/*
   class Solution {
   public:
   int findFirst(vector<int>&nums, int target)
         {
           // to find the first occurence
+          int low = 0, high = nums.size() - 1, ans = -1;
+          while (low <= high)
+          {
+                  int mid = low + (high - low)/2;
+                  if(nums[mid]==target)
+                  {
+                          ans = mid;
+                          high = mid - 1;
+                  }
+                  else if(nums[mid] <  target) low = mid+1;
+                  else high = mid - 1;
+          }
+          return ans;
         }
 
         int findLast(vector<int>&nums, int target)
         {
           // to find the last occurence
+          int low = 0, high = nums.size() - 1, ans = -1;
+          while(low <= high)
+          {
+                  int mid = low + (high - low)/2;
+                  if(nums[mid]==target)
+                  {
+                          ans = mid;
+                          low = mid + 1;
+                  }
+                  else if(nums[mid] < target) low = mid + 1;
+                  else high = mid - 1;
+          }
+          return ans; 
         }
     vector<int> searchRange(vector<int>& nums, int target) {
-        find
+        return {findFirst(nums, target), findLast(nums, target)};
     }
 };
   
-
-*/
