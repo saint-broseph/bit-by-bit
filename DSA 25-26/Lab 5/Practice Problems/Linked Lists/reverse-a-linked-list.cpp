88baa3aa8bafebea4e8611f89a022caa150c2057
Tanishq Sahu <sahutanishq06@gmail.com>
2026-02-25 11:37:56 +0530

    Create reverse-a-linked-list.cpp

---
+++ DSA 25-26/Lab 5/Practice Problems/Linked Lists/reverse-a-linked-list.cpp
@@ -0,0 +1,30 @@
+/*
+  Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
+        while(curr!=nullptr)
+        {
+            ListNode *nextTemp = curr ->  next; //store next node
+            curr -> next = prev; // flip the pointer
+            prev = curr; // move prev forward
+            curr = nextTemp; // move curr forward
+        }
+
+        return prev;
+    }
+};
